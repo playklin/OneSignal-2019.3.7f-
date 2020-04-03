0c7b21c0b85b772aa7ab786f7d7c7ccb65db55c5
Minakov <playklin@gmail.com>
2020-04-03 14:09:30 +0300

    рабочая версия

---
+++ Assets/Scenes/mainOneSignal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainOneSignal : MonoBehaviour
+{
+    void Start () {
+    // Enable line below to enable logging if you are having issues setting up OneSignal. (logLevel, visualLogLevel)
+    // OneSignal.SetLogLevel(OneSignal.LOG_LEVEL.INFO, OneSignal.LOG_LEVEL.INFO);
+  
+    OneSignal.StartInit("18acf8c9-f31d-485a-92ab-8d7b886be084")
+    .HandleNotificationOpened(HandleNotificationOpened)
+    .EndInit();
+  
+     OneSignal.inFocusDisplayType = OneSignal.OSInFocusDisplayOption.Notification;
+    }
+
+    // Gets called when the player opens the notification.
+    private static void HandleNotificationOpened(OSNotificationOpenedResult result) {
+    }
+}
