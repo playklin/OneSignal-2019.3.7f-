4032f5de02d2a1b9987d5876f21039406a86a122
Minakov <playklin@gmail.com>
2020-04-03 14:10:40 +0300

    дописка

---
+++ Assets/Scenes/PushOScreate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Net;
+using System.Text;
+
+public class PushOScreate : MonoBehaviour
+{
+    /*
+    var request = WebRequest.Create("https://onesignal.com/api/v1/notifications") as HttpWebRequest;
+
+    request.KeepAlive = true;
+    request.Method = "POST";
+    request.ContentType = "application/json; charset=utf-8";
+
+    request.Headers.Add("authorization", "Basic MzIyMWJiZjMtYjU0ZC00ZDBhLTk1MjktODNmNTY2MzlhYjVj");
+
+    byte[] byteArray = Encoding.UTF8.GetBytes("{"
+                                        + "\"app_id\": \"18acf8c9-f31d-485a-92ab-8d7b886be084\","
+                                        + "\"contents\": {\"en\": \"English Message\"},"
+                                        + "\"included_segments\": [\"All\"]}");
+
+    string responseContent = null;
+
+    try {
+    using (var writer = request.GetRequestStream()) {
+        writer.Write(byteArray, 0, byteArray.Length);
+    }
+
+    using (var response = request.GetResponse() as HttpWebResponse) {
+        using (var reader = new StreamReader(response.GetResponseStream())) {
+            responseContent = reader.ReadToEnd();
+        }
+    }
+    }
+    catch (WebException ex) {
+    System.Diagnostics.Debug.WriteLine(ex.Message);
+    System.Diagnostics.Debug.WriteLine(new StreamReader(ex.Response.GetResponseStream()).ReadToEnd());
+    }
+
+    System.Diagnostics.Debug.WriteLine(responseContent);
+    */
+}
+
---
+++ Assets/Scenes/PushTEST.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;using System.Collections.Generic;
+using UnityEngine;using UnityEngine.UI;
+using System;
+//using SimpleJSON; 
+using System.IO;
+using UnityEngine.Networking;using UnityEngine.SceneManagement;
+
+public class PushTEST : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         StartCoroutine(PushNot("1"));
+    }
+    //public void ClickPush(){StartCoroutine(PushNot(1));}
+    //public void ClickPush(){SceneManager.LoadScene("M1");}
+
+    IEnumerator PushNot(string id){WWWForm form = new WWWForm(); 
+         form.AddField("id", id);
+        //form.AddField("_phone", phone);form.AddField("_email", email);
+        using (UnityWebRequest www = UnityWebRequest.Post("http://p905504y.beget.tech/notification1.php",form))
+        {yield return www.SendWebRequest(); if (www.isNetworkError || www.isHttpError) { Debug.Log(www.error);}else{
+        //g_save_ok.SetActive(true);
+        //t_title_order.text = www.downloadHandler.text;
+        //PlayerPrefs.SetString("phone", www.downloadHandler.text);
+        Debug.Log("" + www.downloadHandler.text);
+        }}
+    }
+}
